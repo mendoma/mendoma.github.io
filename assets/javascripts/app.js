3249adcdd53c0a5d20e728d7a3e5a28bf9a3b578
Manny Mendoza <manny17t@gmail.com>
2018-10-08 12:23:46 -0700

    Materialize CSS included

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,3 +1,9 @@
-$('.special.cards .image').dimmer({
-    on: 'hover'
-  })
+// document.addEventListener('DOMContentLoaded', function () {
+//   var elems = document.getElementsByClassName('sidenav')
+//   let options
+//   var instances = M.Sidenav.init(elems, options);
+// });
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
