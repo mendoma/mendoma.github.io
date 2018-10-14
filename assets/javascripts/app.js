63efa381eb735491cc8fb45a54ca64932833d306
Manny Mendoza <manny17t@gmail.com>
2018-10-13 21:02:37 -0700

    Updated with materialize

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,9 +1,25 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.getElementsByClassName('sidenav')
-//   let options
-//   var instances = M.Sidenav.init(elems, options);
-// });
+// Show sidebar on smaller screens
+$(document).ready(function () {
 
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
+  // Show sidebar on smaller screens
+  $('.sidenav').sidenav()
+
+  // Trigger modal
+  $('.modal-trigger').on('click', function (element) {
+
+    // Prevent default link behavior
+    element.preventDefault()
+
+    // Set iframe source in modal to project github link
+    let src = $(this).attr('link')
+    let code = $(this).attr('code')
+
+    $('#modal-frame').attr('src', src)
+    $('#deployed-site').attr('href', src)
+    $('#view-code').attr('href', code)
+
+    // Trigger modal
+    $('.modal').modal()
+
+  })
+})
