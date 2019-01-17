e3956d21a249307c3657d9019601e9b216ca1c9c
Manny Mendoza <manny17t@gmail.com>
2019-01-16 17:06:42 -0800

    Modal working again

---
+++ build/precache-manifest.137dbbae6ac624fbe0a54d244407c950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d4cc4f63df1b1b91d34f4bf9f7cb843",
+    "url": "/static/media/king_piece.0d4cc4f6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6688cce32e3467d2fa1",
+    "url": "/static/js/main.f6688cce.chunk.js"
+  },
+  {
+    "revision": "5f79056bdc79c7420a75",
+    "url": "/static/js/1.5f79056b.chunk.js"
+  },
+  {
+    "revision": "f6688cce32e3467d2fa1",
+    "url": "/static/css/main.3f419d35.chunk.css"
+  },
+  {
+    "revision": "5f79056bdc79c7420a75",
+    "url": "/static/css/1.7cdf0eca.chunk.css"
+  },
+  {
+    "revision": "0c02fd90aaa698b8af8a6628bb6188cb",
+    "url": "/index.html"
+  }
+];
